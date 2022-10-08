a959b5c9e25569feba1bf3770f60f517af85596c
Karl Satchi Navida <satchi5600@gmail.com>
2022-10-09 00:44:48 +0800

    Update 20221009 - Foundation Laying
    + Installed NPM for node related actions and development
    + Installed React for User Frontend
    + Installed initial required libraries and packages
    + Initial commit of all files

---
+++ resources/js/scripts.js
@@ -0,0 +1,28 @@
+try {
+	// jQuery
+	window.$ = window.jQuery = require('jquery');
+
+	// popper.js
+	window.Popper = require('@popperjs/core');
+
+	// Bootstrap 4
+	require('bootstrap/dist/js/bootstrap.min')
+	require('bootstrap-select/dist/js/bootstrap-select.min')
+
+	// Sweetalert 2
+	window.Swal = require('sweetalert2/dist/sweetalert2.all.min');
+
+	// // Slick Carousel
+	// window.slick = require('slick-carousel/slick/slick.min');
+
+	// Fontawesome 6
+	require('@fortawesome/fontawesome-free/js/solid.min.js');
+	require('@fortawesome/fontawesome-free/js/regular.min.js');
+	require('@fortawesome/fontawesome-free/js/brands.min.js');
+	require('@fortawesome/fontawesome-free/js/fontawesome.min.js');
+
+	// Summernote
+	require('summernote/dist/summernote-bs4.min');
+} catch (e) {
+	console.error(e);
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+	</head>
+
+	<body>
+		<div id="example"></div>
+		
+		<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+	</body>
+</html>
