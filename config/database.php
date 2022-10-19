2d9883f18dc4403920e234d69f552072ce4ea09f
Karl Satchi Navida <satchi5600@gmail.com>
2022-10-19 10:59:03 +0800

    Update 20221019 - PHPUnit Update

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
 		'mysql_test' => [
 			'driver' => 'mysql',
 			'url' => env('DATABASE_URL'),
-			'host' => env('DB_HOST', 'mysql'),
+			'host' => env('DB_HOST', '127.0.0.1'),
 			'port' => env('DB_PORT', '3306'),
 			'database' => env('DB_DATABASE', 'forge'),
 			'username' => env('DB_USERNAME', 'forge'),
