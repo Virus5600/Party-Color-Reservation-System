9ada10c5669dd1c140cbb4c24fea852dcd94d9f8
Karl Satchi Navida <satchi5600@gmail.com>
2022-10-19 10:39:00 +0800

    Update 20221019 - PHPUnit Update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-	use RefreshDatabase;
+	use DatabaseMigrations;
 
 	/**
 	 * A basic test example.
