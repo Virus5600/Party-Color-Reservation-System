17f0ad8b40650b454b13d5fad2cc9865d3202623
Karl Satchi Navida <satchi5600@gmail.com>
2022-10-25 16:32:32 +0800

    Fix 20221025 - Fixed Announcement base64 image issue

---
+++ app/AnnouncementContentImage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnnouncementContentImage extends Model
+{
+	protected $fillable = [
+		'announcement_id',
+		'image_name'
+	];
+
+	// Relation
+	protected function announcement() { return $this->belongsTo('App\Announcement'); }
+
+	// Custom Function
+	public function getImage() {
+		return asset('uploads/announcements/'.$this->announcement_id.'/'.$this->image_name);
+	}
+}
