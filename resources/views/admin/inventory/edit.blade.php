9fa891b9a700feba7c78b15ea4a8105dfa19c489
JM-Elbambo <jm.elbambo27@gmail.com>
2022-11-05 23:31:38 +0800

    Add edit/update functions to Inventory

--- resources/views/admin/inventory/edit.blade.php
+++ resources/views/admin/inventory/edit.blade.php
@@ -10,7 +10,7 @@
 				{{-- Header --}}
 				<div class="col-12">
 					<h1>
-						<a href="javascript:void(0);" onclick="confirmLeave('{{route('admin.announcements.index', ['d' => $show_drafts, 'sd' => $show_softdeletes])}}');" class="text-dark text-decoration-none font-weight-normal">
+						<a href="javascript:void(0);" onclick="confirmLeave('{{route('admin.inventory.index')}}');" class="text-dark text-decoration-none font-weight-normal">
 							<i class="fas fa-chevron-left mr-2"></i>発表
 						</a>
 					</h1>
@@ -22,62 +22,34 @@
 	<hr class="hr-thick">
 
 	<div class="row">
-		<div class="col-12 col-md-8 mx-auto">
+		<div class="mx-auto" style="max-width: 25rem !important; flex: 0 0 25rem;">
 			<div class="card dark-shadow mb-5" id="inner-content">
 				<div class="card-body">
-					<form action="{{ route('admin.announcements.update', [$announcement->id]) }}" method="POST" enctype="multipart/form-data">
-						{{ csrf_field() }}
-						<input type="hidden" name="sd" value="{{ $show_softdeletes }}">
-						<input type="hidden" name="d" value="{{ $show_drafts }}">
-						
-						{{-- ANNOUNCEMENT POSTER --}}
-						<div class="row">
-							<div class="col-12 col-lg-6">
-								<div class="form-group text-center text-lg-left w-100" style="max-height: 20rem;">
-									<label class="h5" for="image">発行のイメージ</label><br>
-									<img src="{{ $announcement->getPoster() }}" class="img-fluid cursor-pointer border" style="border-width: 0.25rem!important; max-height: 16.25rem;" id="image" alt="Announcement Image" data-fallback-image="{{ asset('uploads/announcements/default.png') }}">
-									<input type="file" id="image" name="image" class="d-none" accept=".jpg,.jpeg,.png"><br>
-									<small class="text-muted pt-0 mt-0"><b>初認された形式：</b> JPEG, JPG, PNG, WEBP</small><br>
-									<small class="text-muted pt-0 mt-0"><b>最大サイズ：</b> 5MB</small>
-								</div>
-							</div>
-
-							{{-- TITLE --}}
-							<div class="col-12 col-lg-6">
-								<div class="form-group">
-									<label class="h5" for="title">タイトル</label>
-									<input class="form-control" type="text" name="title" value="{{ $announcement->title }}"/>
-								</div>
-
-								<div class="form-group text-counter-parent">
-									<label class="h5" for="summary">概要</label>
-									<textarea class="form-control not-resizable text-counter-input" name="summary" rows="4" data-max="255">{{ $announcement->summary }}</textarea>
-									<span class="text-counter small">255</span>
-								</div>
-
-								<div class="form-group my-auto">
-									<div class="custom-control custom-switch custom-switch-md">
-										<input type='checkbox' class="custom-control-input" id="is-draft" name="is_draft" {{ $announcement->is_draft ? 'checked' : '' }}/>
-										<label class="custom-control-label pt-1 pl-3" for="is-draft">ドラフトとしてマーク</label>
-									</div>
-								</div>
+					<form action="{{ route('admin.inventory.update', [$item->id]) }}" method="POST" enctype="multipart/form-data">
+						{{csrf_field()}}
+						{{-- ITEM NAME --}}
+						<div class="form-group">
+							<label class="h5" for="title">Item Name</label>
+							<input class="form-control" type="text" name="item_name" value="{{ $item->item_name }}"/>
+						</div>
+						{{-- QUANTITY --}}
+						<div class="form-group">
+							<label class="h5" for="title">Quantity</label>
+							<input class="form-control" type="number" min="0" max="4294967295" name="quantity" value="{{ $item->quantity }}"/>
+						</div>
+						{{-- IS ACTIVE --}}
+						<div class="form-group my-auto">
+							<div class="custom-control custom-switch custom-switch-md">
+								<input type='checkbox' class="custom-control-input image-input-switch" name="is_active" id="is_active"/>
+								<label class="custom-control-label pt-1 pl-3" for="is_active">Set Active?</label>
 							</div>
 						</div>
 
 						<hr class="hr-thick">
 
-						<div class="row">
-							<div class="col">
-								<label class="h5" for="content">内容</label>
-								<textarea class="summernote" name="content" rows="5">{!! $announcement->content !!}</textarea>
-							</div>
-						</div>
-
 						<div class="row py-3">
-							<div class="col">
-								<button class="btn btn-success ml-auto" type="submit" data-action="submit">提出する</button>
-								<a href="javascript:void(0);" onclick="confirmLeave('{{route('admin.announcements.index', ['d' => $show_drafts, 'sd' => $show_softdeletes])}}');" class="btn btn-danger ml-3 mr-auto">キャンセル</a>
-							</div>
+							<button class="btn btn-success ml-auto" type="submit" data-action="submit">Submit</button>
+							<a href="javascript:void(0);" onclick="confirmLeave('{{route('admin.inventory.index')}}');" class="btn btn-danger ml-3 mr-auto">Cancel</a>
 						</div>
 					</form>
 				</div>
@@ -89,76 +61,12 @@
 
 @section('css')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/util/custom-switch.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('css/util/text-counter.css') }}" />
-<style type="text/css">
-	.note-toolbar {
-		display: flex;
-		flex-wrap: wrap;
-		align-content: center;
-	}
-</style>
 @endsection
 
 @section('scripts')
-<script type="text/javascript" src="{{ asset('js/util/text-counter.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/util/confirm-leave.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/util/disable-on-submit.js') }}"></script>
 <script type="text/javascript">
-	function openInput(obj) {
-		$("[name=" + obj.attr("id") + "]").trigger("click");
-	}
-
-	function swapImg(obj) {
-		if (obj.files && obj.files[0]) {
-			let reader = new FileReader();
-
-			reader.onload = function(e) {
-				$("#image").attr("src", e.currentTarget.result);
-			}
-
-			reader.readAsDataURL(obj.files[0])
-		}
-		else {
-			$("#image").attr("src", "{{ $announcement->getPoster() }}");
-		}
-	}
-
-	$(document).ready(function() {
-		// Profile Image Changing
-		$("#image").on("click", function() {openInput($(this))});
-		$("[name=image]").on("change", function() {swapImg(this)});
-
-		// Summernote
-		$('.summernote').summernote({
-			lang: 'ja-JP',
-			minHeight: 128,
-			maxHeight: 384,
-			height: 256,
-			placeholder: 'ここに内容が入れます。。。',
-			toolbar: [
-				['style', ['style']],
-				['font', ['bold', 'underline', 'clear']],
-				['fontname', ['fontname', 'fontsize']],
-				['color', ['color']],
-				['para', ['ul', 'ol', 'paragraph']],
-				['insert', ['link', 'hr', 'picture', 'video']],
-				['view', ['fullscreen', 'codeview', 'help']],
-				['history', ['undo', 'redo']]
-			],
-			popover: {
-				image: [
-					['image', ['resizeFull', 'resizeHalf', 'resizeQuarter', 'resizeNone']],
-					['float', ['floatLeft', 'floatRight', 'floatNone']],
-					['remove', ['removeMedia']]
-				],
-				air: [
-					['insert', ['link']],
-				],
-				link: [
-					['link', ['linkDialogShow', 'unlink']]
-				]
-			}
-		});
-	});
+    $(document).ready(() => { $("#is_active").prop("checked", {{ !$item->trashed() }}); });
 </script>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,69 +96,70 @@
 					</div>
 				</div>
 			</main>
-
-			<!-- SCRIPTS -->
-			<script type="text/javascript">const fiFallbackImage = '{{ asset("uploads/users/default.png") }}';</script>
-			<script type="text/javascript" src="{{ asset('js/admin.js') }}"></script>
-			<script type="text/javascript" src="{{ asset('js/util/fallback-image.js') }}"></script>
-			<script type="text/javascript">
-				@if (Session::has('flash_error'))
-				Swal.fire({
-					{!!Session::has('has_icon') ? "icon: `error`," : ""!!}
-					title: `{{Session::get('flash_error')}}`,
-					{!!Session::has('message') ? 'html: `' . Session::get('message') . '`,' : ''!!}
-					position: {!!Session::has('position') ? '`' . Session::get('position') . '`' : '`top`'!!},
-					showConfirmButton: false,
-					toast: {!!Session::has('is_toast') ? Session::get('is_toast') : true!!},
-					{!!Session::has('has_timer') ? (Session::get('has_timer') ? (Session::has('duration') ? ('timer: ' . Session::get('duration')) . ',' : `timer: 10000,`) : '') : `timer: 10000,`!!}
-					background: `#dc3545`,
-					customClass: {
-						title: `text-white`,
-						content: `text-white`,
-						popup: `px-3`
-					},
-				});
-				@elseif (Session::has('flash_info'))
-				Swal.fire({
-					{!!Session::has('has_icon') ? "icon: `info`," : ""!!}
-					title: `{{Session::get('flash_info')}}`,
-					{!!Session::has('message') ? 'html: `' . Session::get('message') . '`,' : ''!!}
-					position: {!!Session::has('position') ? '`' . Session::get('position') . '`' : '`top`'!!},
-					showConfirmButton: false,
-					toast: {!!Session::has('is_toast') ? Session::get('is_toast') : true!!},
-					{!!Session::has('has_timer') ? (Session::get('has_timer') ? (Session::has('duration') ? ('timer: ' . Session::get('duration')) . ',' : `timer: 10000,`) : '') : `timer: 10000,`!!}
-					background: `#17a2b8`,
-					customClass: {
-						title: `text-white`,
-						content: `text-white`,
-						popup: `px-3`
-					},
-				});
-				@elseif (Session::has('flash_success'))
-				Swal.fire({
-					{!!Session::has('has_icon') ? "icon: `success`," : ""!!}
-					title: `{{Session::get('flash_success')}}`,
-					{!!Session::has('message') ? 'html: `' . Session::get('message') . '`,' : ''!!}
-					position: {!!Session::has('position') ? '`' . Session::get('position') . '`' : '`top`'!!},
-					showConfirmButton: false,
-					toast: {!!Session::has('is_toast') ? Session::get('is_toast') : true!!},
-					{!!Session::has('has_timer') ? (Session::get('has_timer') ? (Session::has('duration') ? ('timer: ' . Session::get('duration')) . ',' : `timer: 10000,`) : '') : `timer: 10000,`!!}
-					background: `#28a745`,
-					customClass: {
-						title: `text-white`,
-						content: `text-white`,
-						popup: `px-3`
-					},
-				});
-				@endif
-
-				$(document).ready(function(){
-					$(".col-a").click(function(){
-						$('.collapse.show').collapse('hide');
-					});
-				});
-			</script>
-			@yield('scripts')
 		</div>
+
+		<!-- SCRIPTS -->
+		<script type="text/javascript">const fiFallbackImage = '{{ asset("uploads/users/default.png") }}';</script>
+		<script type="text/javascript" src="{{ asset('js/admin.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('js/util/fallback-image.js') }}"></script>
+		<script type="text/javascript">
+			@if (Session::has('flash_error'))
+			Swal.fire({
+				{!!Session::has('has_icon') ? "icon: `error`," : ""!!}
+				title: `{{Session::get('flash_error')}}`,
+				{!!Session::has('message') ? 'html: `' . Session::get('message') . '`,' : ''!!}
+				position: {!!Session::has('position') ? '`' . Session::get('position') . '`' : '`top`'!!},
+				showConfirmButton: false,
+				toast: {!!Session::has('is_toast') ? Session::get('is_toast') : true!!},
+				{!!Session::has('has_timer') ? (Session::get('has_timer') ? (Session::has('duration') ? ('timer: ' . Session::get('duration')) . ',' : `timer: 10000,`) : '') : `timer: 10000,`!!}
+				background: `#dc3545`,
+				customClass: {
+					title: `text-white`,
+					content: `text-white`,
+					popup: `px-3`
+				},
+			});
+			@elseif (Session::has('flash_info'))
+			Swal.fire({
+				{!!Session::has('has_icon') ? "icon: `info`," : ""!!}
+				title: `{{Session::get('flash_info')}}`,
+				{!!Session::has('message') ? 'html: `' . Session::get('message') . '`,' : ''!!}
+				position: {!!Session::has('position') ? '`' . Session::get('position') . '`' : '`top`'!!},
+				showConfirmButton: false,
+				toast: {!!Session::has('is_toast') ? Session::get('is_toast') : true!!},
+				{!!Session::has('has_timer') ? (Session::get('has_timer') ? (Session::has('duration') ? ('timer: ' . Session::get('duration')) . ',' : `timer: 10000,`) : '') : `timer: 10000,`!!}
+				background: `#17a2b8`,
+				customClass: {
+					title: `text-white`,
+					content: `text-white`,
+					popup: `px-3`
+				},
+			});
+			@elseif (Session::has('flash_success'))
+			Swal.fire({
+				{!!Session::has('has_icon') ? "icon: `success`," : ""!!}
+				title: `{{Session::get('flash_success')}}`,
+				{!!Session::has('message') ? 'html: `' . Session::get('message') . '`,' : ''!!}
+				position: {!!Session::has('position') ? '`' . Session::get('position') . '`' : '`top`'!!},
+				showConfirmButton: false,
+				toast: {!!Session::has('is_toast') ? Session::get('is_toast') : true!!},
+				{!!Session::has('has_timer') ? (Session::get('has_timer') ? (Session::has('duration') ? ('timer: ' . Session::get('duration')) . ',' : `timer: 10000,`) : '') : `timer: 10000,`!!}
+				background: `#28a745`,
+				customClass: {
+					title: `text-white`,
+					content: `text-white`,
+					popup: `px-3`
+				},
+			});
+			@endif
+
+			$(document).ready(function(){
+				$(".col-a").click(function(){
+					$('.collapse.show').collapse('hide');
+				});
+			});
+		</script>
+		@yield('scripts')
+		
 	</body>
 </html>
