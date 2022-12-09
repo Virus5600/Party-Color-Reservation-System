f6b87dcff36725f7cf2d60320f48a81c6dc0cdd7
JM-Elbambo <jm.elbambo27@gmail.com>
2022-12-09 10:00:05 +0800

    Add static reservation frontend

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
 @extends('layouts.admin')
 
 @section('title', 'Reservations')
@@ -17,7 +19,7 @@
 					<div class="row">
 						{{-- ADD --}}
 						<div class="col-12 col-md-6 text-center text-md-right">
-							<a href="{{ route('admin.reservations.create') }}" class="btn btn-success m-auto"><i class="fa fa-plus-circle mr-2"></i>Add Reservation</a>
+							<a href="@{{ route('admin.reservations.create') }}" class="btn btn-success m-auto"><i class="fa fa-plus-circle mr-2"></i>Add Reservation</a>
 						</div>
 
 						{{-- SEARCH --}}
@@ -30,32 +32,71 @@
 	</div>
 
 	<div class="card dark-shadow overflow-x-scroll flex-fill mb-3" id="inner-content">
-		<table class="table table-striped my-0">
-			<thead>
-				<tr>
-					<th class="text-center">Permission Name</th>
-					<th class="text-center">Number of Permission Users</th>
-					<th class="text-center"></th>
-				</tr>
-			</thead>
+		<div class="container-fluid">
+			{{-- Header --}}
+			<div class="row">
+				<div class="col-2 border d-flex align-items-center justify-content-center bold font-weight-bold">
+					Date and Time
+				</div>
+				<div class="col border">
+					<div class="row border row d-flex align-items-center justify-content-center font-weight-bold py-2">
+						2022 January
+					</div>
+					<div class="row">
+						<?php
+						$days = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+						$firstDayWeek = 24;
+						?>
+						@for ($i = 0; $i < count($days); $i++)
+						<div class="col border d-flex align-items-center justify-content-center text-center font-weight-bold py-1">
+							<?php echo $i + $firstDayWeek; ?><br>
+							(<?php echo $days[$i]; ?>)
+						</div>
+						@endfor
+					</div>
+				</div>
+			</div>
 
-			<tbody id="table-content">
-				@forelse ($permissions as $p)
-				<tr class="enlarge-on-hover">
-					<td class="text-center align-middle mx-auto font-weight-bold">{{ $p->name }}</td>
-					<td class="text-center align-middle mx-auto">{{ $p->allUsers()->count() }}</td>
+			{{-- Time Slots Generation --}}
+			<?php
+			$minutes = 30;
+			$start = "09:00";
+			$end = "17:01";
+			$startDate = DateTime::createFromFormat("H:i", $start);
+			$endDate = DateTime::createFromFormat("H:i", $end);
+			$interval = new DateInterval("PT".$minutes."M");
+			$dateRange = new DatePeriod($startDate, $interval, $endDate);
+			?>
+			
+			<div class="row">
 
-					<td class="align-middle">
-						<a href="{{ route('admin.permissions.show', [$p->slug]) }}" class="btn btn-primary"><i class="fas fa-eye mr-2"></i>View</a>
-					</td>
-				</tr>
-				@empty
-				<tr>
-					<td class="text-center" colspan="3">Nothing to display~</td>
-				</tr>
-				@endforelse
-			</tbody>
-		</table>
+				{{-- Time Slots --}}
+				<div class="col-2">
+					@foreach ($dateRange as $date)
+					<div class="row border d-flex align-items-center justify-content-center">
+						<?php echo $date->format("H:i"); ?>
+					</div>
+					@endforeach
+				</div>
+
+				{{-- Reservations --}}
+				@for ($i = 0; $i < count($days); $i++)
+				<div class="col">
+					@foreach ($dateRange as $date)
+					{{-- TODO: If reservation exists 
+					<div class="row border d-flex align-items-center justify-content-center">
+						<i class="bi bi-circle text-danger"></i><br>
+					</div>
+					--}}
+					{{-- TODO: else --}}
+					<div class="row border d-flex align-items-center justify-content-center bg-light">
+						<i class="bi bi-x text-secondary"></i><br>
+					</div>
+					@endforeach
+				</div>
+				@endfor
+			</div>
+		</div>
 	</div>
 </div>
 @endsection
