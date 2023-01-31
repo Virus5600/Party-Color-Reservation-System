6b280c9b66d874f259085cf96b4e1dcb3f136499
KrimssmirK <krimssmirk003@gmail.com>
2023-02-01 01:48:50 +0800

    added reservation success ui

--- resources/js/components/Reservation.js
+++ resources/js/components/Reservation.js
@@ -13,11 +13,16 @@ import App from './App';
 
 // icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPerson, faEnvelope, faPhone, faCalendar, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faPerson, faEnvelope, faPhone, faCalendar, faClock, faCircleCheck, faClipboard, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
+
+
+
+// root
 const Reservation = () => {
- 
+
 	const [isReturnClicked, setReturnClicked] = useState(false);
+
 
 	const handleClickReturn = () => {
 		setReturnClicked(true);
@@ -33,9 +38,52 @@ const Reservation = () => {
 				</div>
 
 				<ReservationForm onClickReturn={handleClickReturn} />
+
 			</div>
 		);
 	}
+};
+
+// under development
+const ReservationComfirmed = ({ onClickReturn, number, date, time }) => {
+	return (
+		<div className='ReservationConfirmed'>
+			<div className='ReservationConfirmed-success'>
+
+				<FontAwesomeIcon icon={faCircleCheck} className='ReservationConfirmed-success-icon' />
+
+				<div>
+					<span className='ReservationConfirmed-success-title'>Your reservation has been confirmed!</span><br />
+					<span>An email confirmation has been sent to you.</span><br />
+					<span>Confirmation #<span>001</span></span>
+				</div>
+			</div>
+			<div className='ReservationConfirmed-details'>
+				<div className='ReservationConfirmed-details-content'>
+
+					<div className='ReservationConfirmed-details-left'>
+						<DetailsRight date={date} time={time} number={number} />
+					</div>
+
+					{/* <div className='ReservationConfirmed-details-right'>
+						<div className='ReservationConfirmed-details-right-button'>
+							<FontAwesomeIcon icon={faClipboard} className='ReservationConfirmed-details-right-icon' />
+							<span>Modify</span>
+						</div>
+						<div className='ReservationConfirmed-details-right-button'>
+							<FontAwesomeIcon icon={faCircleXmark} className='ReservationConfirmed-details-right-icon' />
+							<span>Cancel</span>
+						</div>
+					</div> */}
+
+				</div>
+				<div className='ReservationConfirmed-details-button'>
+					<button onClick={onClickReturn} >Close</button>
+				</div>
+
+			</div>
+		</div>
+	);
 };
 
 // temporary data
@@ -46,18 +94,13 @@ const times = [
 	{ id: 4, time: '8:00PM' },
 ];
 
-const API_FOR_GETTING_AVAILABLE_TIME_BASED_ON_DATE = '';
-
-const ReservationForm =	({ onClickReturn }) => {
+const ReservationForm = ({ onClickReturn }) => {
 
 	const [isTimeClicked, setTimeClicked] = useState(false);
 	const [number, setNumber] = useState(1);
 	const [date, setDate] = useState('');
 	const [time, setTime] = useState('');
-
-	// console.log('number:', number);
-	// console.log('date:', date);
-	// console.log('time:', time);
+	const [success, setSuccess] = useState(false);
 
 	const selectedStyle = { color: 'red' };
 	const unselectedStyle = { color: 'black' };
@@ -71,28 +114,35 @@ const ReservationForm =	({ onClickReturn }) => {
 		setNumber(number);
 		setDate(date);
 	};
-		
 
-	return (
-		<div className='Reservation-form'>
-			<h2>Reservation at Party Color</h2>
-			<div className='Reservation-details'>
-				<ol className='d-flex'>
-					<li style={ !isTimeClicked ? selectedStyle : unselectedStyle }>Select a Date</li>
-					<li style={ isTimeClicked ? selectedStyle : unselectedStyle }>Additional details</li>
-				</ol>
-				<hr />
-				{ !isTimeClicked ? <Dates onFindTableClick={handleClickFindTable} onTimeClicked={handleTimeClick} /> : <Details number={number} date={date} time={time} /> }
-					
+	if (success) {
+		return <ReservationComfirmed onClickReturn={onClickReturn} number={number} date={date} time={time} />;
+	} else {
+		return (
+			<div>
+				<div className='Reservation-form'>
+					<h2>Reservation at Party Color</h2>
+					<div className='Reservation-details'>
+						<ol className='d-flex'>
+							<li style={!isTimeClicked ? selectedStyle : unselectedStyle}>Select a Date</li>
+							<li style={isTimeClicked ? selectedStyle : unselectedStyle}>Additional details</li>
+						</ol>
+						<hr />
+						{!isTimeClicked ? <Dates onFindTableClick={handleClickFindTable} onTimeClicked={handleTimeClick} /> : <Details number={number} date={date} time={time} setSuccess={setSuccess} />}
+
+					</div>
+					<button onClick={onClickReturn} className='return-button'>Return</button>
+				</div>
 			</div>
-			<button onClick={onClickReturn} className='return-button'>Return</button>
-		</div>	 
-	);
+
+		);
+	}
+
 };
 
 
 
-const Details = ({ number, date, time }) => {
+const Details = ({ number, date, time, setSuccess }) => {
 	const [fullName, setFullName] = useState('');
 	const [email, setEmail] = useState('');
 	const [phoneNumber, setPhoneNumber] = useState('');
@@ -100,36 +150,27 @@ const Details = ({ number, date, time }) => {
 	const [specialRequest, setSpecialRequest] = useState('');
 	const token = document.querySelector('meta[name=csrf-token]').content;
 
-	// console.log('full name:', fullName);
-	// console.log('email:', email);
-	// console.log('phone number:', phoneNumber);
-	// console.log('check update:', isCheckedUpdate);
-	// console.log('special request:', specialRequest);
-
-
-	// eto ung url na para sa api (sending to backend the reservation details)
 	const API_TO_SEND_RESERVATION = 'api/react/reservations/create';
 
 	const handleClickServeButton = async () => {
-		/**
-		 * Still has missing required parameters:
-		 * reservation_date [string date] - The day that they set for their reservation
-		 * pax [integer] - Amount of people that will attend
-		 * price [double] - Sum of the pricesfor the menu they've picked
-		 * time_hour [integer] - Start hour time (24 hour format)
-		 * time_min [integer] - Start minute time
-		 * reservation_time [string time] - Combined time_hour and time_min to create the H:i (i.e.: 17:30) time format
-		 * extension [double] - Time extension they would like to avail (in hours. So 1 hour and 30 minutes will be 1.5 hours). This is optional.
-		 * menu [integer array] - 
-		 */
+		let flag;
 		const result = await axios.post(API_TO_SEND_RESERVATION, {
 			_token: token,						// _token [string] - For CSRF prevention
-			contact_name: [fullName],			// contact_name [string array] - Primary contact person (Can be many)
-			email: [email],						// email [string array] - Primary contact email (Can be many)
-			phone_numbers: [phoneNumber],		// phone_numbers [string array] - A list of phone numbers that can be contacted
-			specialRequest: specialRequest,		// special_request [string] - Not yet implemented
-			subscribed: isCheckedUpdate,		// subscribed [boolean] = Not yet implemented
+			reservation_date: date,
+			pax: Number(number),
+			price: 3000,  // static as of now
+			time_hour: Number(time.split(':')[0]) + 12,
+			time_min: 0,  // static as of now
+			reservation_time: Number(time.split(':')[0]),
+			extension: 0, // static as of now
+			menu: [1], // static as of now since plan has only one
+			contact_name: [fullName],
+			contact_email: [email],
+			phone_numbers: phoneNumber,
+			specialRequest: specialRequest,		// Not yet implemented (backend)
+			subscribed: isCheckedUpdate,		// Not yet implemented (backend)
 		}).then(response => {
+			if (response.data.success) flag = true;
 			if (response.success) {
 				Swal.fire({
 					title: response.flash_success,
@@ -144,6 +185,8 @@ const Details = ({ number, date, time }) => {
 						popup: `px-3`
 					},
 				});
+				console.log(response);
+
 			}
 			else {
 				if (response.type == 'error') {
@@ -160,60 +203,79 @@ const Details = ({ number, date, time }) => {
 							popup: `px-3`
 						},
 					})
+					console.log(response);
+
 				}
 				else if (response.type == 'validation') {
 					// IF VALIDATION FAILED
 					console.log(response.errors);
+
 				}
 			}
+
 		});
+
+		if (flag) setSuccess(true);
 	};
 
 	return (
 		<>
 			<div className='Details'>
-				<DetailsLeft 
+				<DetailsLeft
 					onChangeFullName={setFullName}
 					onChangeEmail={setEmail}
 					onChangePhoneNumber={setPhoneNumber}
 					onCheckUpdate={setCheckedUpdate}
 				/>
-				<DetailsRight number={number} date={date} time={time} onChangeSpecialRequest={setSpecialRequest}/>
+				<DetailsRight number={number} date={date} time={time} specialRequest={true} onChangeSpecialRequest={setSpecialRequest} />
 			</div>
 			<button className='Details-reserve-button' onClick={handleClickServeButton}>RESERVE</button>
 		</>
 	);
 };
 
-const DetailsRight = ({ number, date, time, onChangeSpecialRequest }) => {
-	const handleChangeSpecialRequest = (event) => {
-		onChangeSpecialRequest(event.target.value);
-	};
+function addTwoHours(time) {
+	const selectedTime = Number(time[0]);
+	return (selectedTime + 2) + time.substring(1);
+};
 
-	function addTwoHours(time) {
-		const selectedTime = Number(time[0]);
-		return (selectedTime + 2) + time.substring(1);
+const DetailsRight = ({ number, date, time, ...others }) => {
+
+	const handleChangeSpecialRequest = (event) => {
+		others.onChangeSpecialRequest(event.target.value);
 	};
 
 	return (
 		<div className='Details-right'>
-				<h2>Party Color</h2>
-				<div className='Details-right-item'>
-					<FontAwesomeIcon className='Details-right-icon' icon={faCalendar} />
-					<span>{date}</span>
-				</div>
-				<div className='Details-right-item'>
-					<FontAwesomeIcon className='Details-right-icon' icon={faClock} />
-					<span>{time} - {addTwoHours(time)}</span>
-				</div>
-				<div className='Details-right-item'>
-					<FontAwesomeIcon className='Details-right-icon' icon={faPerson} />
-					<span>{number} people</span>
-				</div>
+			<h2>Party Color</h2>
+
+			<div className='Details-right-item'>
+				<FontAwesomeIcon className='Details-right-icon' icon={faCalendar} />
+				<span>{date}</span>
+			</div>
+
+			<div className='Details-right-item'>
+				<FontAwesomeIcon className='Details-right-icon' icon={faClock} />
+				<span>{time} - {addTwoHours(time)}</span>
+			</div>
+
+			<div className='Details-right-item'>
+				<FontAwesomeIcon className='Details-right-icon' icon={faPerson} />
+				<span>{number} people</span>
+			</div>
+
+
+			{others.specialRequest ?
+				<div>
 					<hr />
 					<input type='text' placeholder='Add special request(optional)' onChange={handleChangeSpecialRequest} />
+				</div>
+				:
+				null
+			}
 
-			</div>
+
+		</div>
 	);
 };
 
@@ -237,24 +299,24 @@ const DetailsLeft = ({
 	};
 	return (
 		<div className='Details-left'>
-				<div className='Details-left-input '>
-					<FontAwesomeIcon className='Details-left-icon adjustment' icon={faPerson} />
-					<input type='text' placeholder='Full name' onChange={handleChangeFullName}/><br />
-				</div>
-
-				<div className='Details-left-input'>
-					<FontAwesomeIcon className='Details-left-icon' icon={faEnvelope} />
-					<input type='email' placeholder='Email' onChange={handleChangeEmail}/><br />
-				</div>
-				<div className='Details-left-input'>
-					<FontAwesomeIcon className='Details-left-icon' icon={faPhone} />
-					<input type='text' placeholder='Phone number' onChange={handleChangePhoneNumber} /><br />
-				</div>
-				<div className='Details-left-input'>
-					<input type="checkbox" id="update" name="update" onClick={handleCheckUpdate}/>
-					<label htmlFor="update">Yes, I want to get email and text updates about my reservation</label><br />
-				</div>
+			<div className='Details-left-input '>
+				<FontAwesomeIcon className='Details-left-icon adjustment' icon={faPerson} />
+				<input type='text' placeholder='Full name' onChange={handleChangeFullName} /><br />
 			</div>
+
+			<div className='Details-left-input'>
+				<FontAwesomeIcon className='Details-left-icon' icon={faEnvelope} />
+				<input type='email' placeholder='Email' onChange={handleChangeEmail} /><br />
+			</div>
+			<div className='Details-left-input'>
+				<FontAwesomeIcon className='Details-left-icon' icon={faPhone} />
+				<input type='text' placeholder='Phone number' onChange={handleChangePhoneNumber} /><br />
+			</div>
+			<div className='Details-left-input'>
+				<input type="checkbox" id="update" name="update" onClick={handleCheckUpdate} />
+				<label htmlFor="update">Yes, I want to get email and text updates about my reservation</label><br />
+			</div>
+		</div>
 	);
 };
 
@@ -272,7 +334,7 @@ const Dates = ({ onFindTableClick, onTimeClicked }) => {
 		setDate(convertDateFormat(new Date(event.target.value)));
 	};
 
-	const handleClickFindTable = async (selectedDate) =>	{
+	const handleClickFindTable = async (selectedDate) => {
 		// try {
 		//	 const data = await axios.get(API_FOR_GETTING_AVAILABLE_TIME_BASED_ON_DATE);
 		// } catch (e) {
@@ -287,6 +349,7 @@ const Dates = ({ onFindTableClick, onTimeClicked }) => {
 		let month = current_date.getMonth() + 1;
 		if (month < 10) month = '0' + month.toString();
 		let date = current_date.getDate();
+		if (Number(date) < 10) date = '0' + date;
 		let compatibleDateFormat = year + '-' + month + '-' + date;
 		return compatibleDateFormat;
 	};
@@ -294,12 +357,12 @@ const Dates = ({ onFindTableClick, onTimeClicked }) => {
 
 	return (
 		<div className='Dates'>
-				<input type='number' min='1' value={number} onChange={handleClickNumber}/>
-				<input type='date' min={date} value={date} onChange={handleClickDate}/>
-				<button className='find-table-button' onClick={handleClickFindTable}>Find a table</button>
+			<input type='number' min='1' value={number} onChange={handleClickNumber} />
+			<input type='date' min={date} value={date} onChange={handleClickDate} />
+			<button className='find-table-button' onClick={handleClickFindTable}>Find a table</button>
 
-				{ isTimeAvailable ? <TimeList times={times} onTimeClicked={onTimeClicked}/> : null }
-				
+			{isTimeAvailable ? <TimeList times={times} onTimeClicked={onTimeClicked} /> : null}
+
 		</div>
 	);
 };
@@ -307,7 +370,7 @@ const Dates = ({ onFindTableClick, onTimeClicked }) => {
 const TimeList = ({ times, onTimeClicked }) => {
 	return (
 		<div className='TimeList'>
-			{ times.map(time =>	<Time key={time.id} time={time.time} onTimeClicked={onTimeClicked}/>) }
+			{times.map(time => <Time key={time.id} time={time.time} onTimeClicked={onTimeClicked} />)}
 		</div>
 	);
 };
@@ -322,7 +385,7 @@ const Time = ({ time, onTimeClicked }) => {
 			<button onClick={handleClickTime}>{time}</button>
 		</div>
 	);
-	
+
 };
 
 
