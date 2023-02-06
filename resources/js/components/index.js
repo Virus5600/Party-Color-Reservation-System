1fcf0943ac89f584cfa26c7428145e4cd425aa18
Virus5600 <satchi5600@gmail.com>
2023-02-07 02:35:47 +0800

    Fucking Fix 20230207 - Fixed various bullcraps
    * Updated packge-lock
    * Updated axios
    * Downgraded react and react-dom to 16.14.0 to match node version and overall project compatibility
    * Updated the fucking components/index.js to adapt and use React 16
    * Rearranged scripts.js' script initialization
    - (Temporarily) removed @testing-library/jest-dom
    - (Temporarily) removed @testing-library/react
    - (Temporarily) removed @testing-library/user-event
    
    IMPORTANT NOTE FOR @KrimssmirK:
    > CHECK if other components are still working after REACT DOWNGRADE
    > When re-adding the `@testing-library` packages, add the `--save-dev` to only compile the scripts on development environment
    > Change Node version to "v18.12.1"
    > Change NPM version to "8.19.2"
    
    Co-Authored-By: KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(
+const root = document.getElementById('app');
+render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>
+	</React.StrictMode>,
+	root
 );
 
 // If you want to start measuring performance in your app, pass a function
