be8ac1ed481eab52892c8eb4469e03c64e882473
KrimssmirK <krimssmirk003@gmail.com>
2023-02-14 15:20:58 +0800

    modifed root

---
+++ resources/js/components/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import ReactDOM from 'react-dom';
+
+// components (pages)
+import Home from './home';
+import Nav from './navigation';
+import TempReservation from './tempreservation';
+import Announcement from './announcement';
+import AnnouncementContent from './announcement/announcementcontent';
+import AboutUs from './aboutus';
+
+import {
+    loaderLatest as loaderQuickAnnouncement,
+    loader as loaderAnnouncement,
+} from './announcement/index';
+
+import {
+    loader as loaderAnnouncementContent
+} from './announcement/announcementcontent/index';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Nav />,
+        children: [
+            {
+                path: 'home',
+                element: <Home />,
+                loader: loaderQuickAnnouncement,
+
+            },
+            {
+                path: 'reservation',
+                element: <TempReservation />,
+            },
+            {
+                path: 'announcement',
+                element: <Announcement />,
+                loader: loaderAnnouncement,
+            },
+            {
+                path: 'announcement/:announcementId',
+                element: <AnnouncementContent />,
+                loader: loaderAnnouncementContent,
+            },
+            {
+                path: 'aboutus',
+                element: <AboutUs />,
+            },
+        ],
+    },
+], {
+    // basename: '/home/',
+});
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+    document.getElementById('app')
+);
+
