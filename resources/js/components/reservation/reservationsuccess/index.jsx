157995333c4d24245bdbdddfc88fd10974294a61
KrimssmirK <krimssmirk003@gmail.com>
2023-02-26 17:17:39 +0800

    added ReservationStatus component

--- resources/js/components/reservation/reservationsuccess/index.jsx
+++ resources/js/components/reservation/reservationsuccess/index.jsx
@@ -1,66 +1,39 @@
+// react
+import React from 'react';
+
+
+// css style
 import './style.css';
 
-// icons
+
+// FontAwesome Icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+
 // react router
-import { Link, useNavigate, useLoaderData } from 'react-router-dom';
-
-import { useEffect, useRef } from 'react';
-
-export function loader() {
-    let reservationstatus = sessionStorage.getItem('reservationsuccess') || sessionStorage.getItem('reservationcancel');
-    reservationstatus = JSON.parse(reservationstatus);
-    console.log('reservationstatus:', reservationstatus);
-    if (reservationstatus == null) {
-        // console.log('inside of if statement');
-        // navigate(-1);
-        // console.log('after navigate function');
-        throw Error();
-    }
-
-    return reservationstatus;
-
-}
+import { Link } from 'react-router-dom';
 
 
-export default function ReservationSuccess({ title, description, linkLabel, link, backgroundStyle, iconStyle, isSuccess }) {
-    // const navigate = useNavigate();
-    const _ = useLoaderData();
-    const isMounted = useRef(false);
-
-
-    useEffect(() => {
-        if (!isMounted.current) {
-            isMounted.current = true;
-        } else {
-            sessionStorage.removeItem('reservationsuccess');
-            sessionStorage.removeItem('reservationcancel');
-        }
-
-    }, []);
-
-    // useEffect(() => {
-    //     let reservationstatus = isSuccess ? sessionStorage.getItem('reservationsuccess') : sessionStorage.getItem('reservationcancel');
-    //     reservationstatus = JSON.parse(reservationstatus);
-    //     console.log('reservationstatus:', reservationstatus);
-    //     if (reservationstatus == null) {
-    //         console.log('inside of if statement');
-    //         navigate(-1);
-    //         console.log('after navigate function');
-    //     }
-    //     sessionStorage.removeItem('reservationstatus');
-    // }, []);
+/**
+ * props
+ * - title
+ * - description
+ * - link_label
+ * - link
+ * - bg_style
+ * - icon_style
+ */
+export default function ReservationStatus(props) {
 
     return (
         <div className='container container-small'>
-            <div className='ReservationSuccess m-5 p-sm-4 p-3' style={backgroundStyle}>
-                <span><FontAwesomeIcon icon="fa-solid fa-circle-check" className='ReeservationSuccess-icon' style={iconStyle} /></span><br />
-                <span>{title}</span><br />
-                <span>{description}</span>
+            <div className='ReservationSuccess m-5 p-sm-4 p-3' style={props.bg_style}>
+                <span><FontAwesomeIcon icon="fa-solid fa-circle-check" className='ReeservationSuccess-icon' style={props.icon_style} /></span><br />
+                <span>{props.title}</span><br />
+                <span>{props.description}</span>
             </div>
             <div className='ReservationSuccess-links'>
-                <Link to={link} className='link'>{linkLabel} another reservation</Link><br />
+                <Link to={props.link} className='link'>{props.link_label}</Link><br />
                 <Link to='/' className='link'>go to Home page</Link>
             </div>
         </div>
